083eaba28513744b804b2114c23acef423c0051a
repetitor <repetitor202@gmail.com>
2021-12-28 20:10:15 +0300

    stepik-cpp

---
+++ stepik-cpp/main_preprocessed.cpp
@@ -0,0 +1,19 @@
+# 1 "main.cpp"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "main.cpp"
+# 1 "square.hpp" 1
+
+
+
+int square(int x);
+# 2 "main.cpp" 2
+
+int main()
+{
+    square(5);
+
+    return 0;
+}
