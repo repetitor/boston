ac3276f50e19b69bcdd3faee66bbd344c52e6aba
repetitor <repetitor202@gmail.com>
2022-02-07 17:32:32 +0300

    up laravel

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# boston
+# boston (laradock)
+
+## up
+```shell
+chmod +x script.sh
+./script.sh -h
+```
+
+## troubles
+### PHPREDIS
+> docker-compose build workspace
+>> Step 101/305 : RUN if [ ${INSTALL_PHPREDIS} = true ]; then     apt-get install -yqq php${LARADOCK_PHP_VERSION}-redis ;fi
+
+solution: WORKSPACE_INSTALL_PHPREDIS=false
+
+### MEMCACHED
+>> Step 231/305 : RUN if [ ${INSTALL_MEMCACHED} = true ]; then   apt-get -y install php${LARADOCK_PHP_VERSION}-igbinary   && apt-get -y install php${LARADOCK_PHP_VERSION}-memcached ;fi
+
+solution: WORKSPACE_INSTALL_MEMCACHED=false
+
+### port is already allocated
+> docker-compose up nginx mariadb
+
+if:
+```
+Creating laradock_workspace_1        ... error
+
+ERROR: for laradock_workspace_1  Cannot start service workspace: driver failed programming external connectivity on endpoint laradock_workspace_1 (a0e69adc2d153e924d6c8b431c87d18bd5efcddf4eac17ce32e59f8d877cf43a): Bind for 0.0.0.0:3000 failed: port is already allocated
+
+ERROR: for workspace  Cannot start service workspace: driver failed programming external connectivity on endpoint laradock_workspace_1 (a0e69adc2d153e924d6c8b431c87d18bd5efcddf4eac17ce32e59f8d877cf43a): Bind for 0.0.0.0:3000 failed: port is already allocated
+ERROR: Encountered errors while bringing up the project.
+```
+
+then:
+```
+WORKSPACE_BROWSERSYNC_HOST_PORT=30000
+```
+
+if:
+```
+Creating laradock_nginx_1       ... error
+
+ERROR: for laradock_nginx_1  Cannot start service nginx: driver failed programming external connectivity on endpoint laradock_nginx_1 (05258db447edfcb766cb16b3895772c972128c4e68608317783257081467492a): Bind for 0.0.0.0:443 failed: port is already allocated
+
+ERROR: for nginx  Cannot start service nginx: driver failed programming external connectivity on endpoint laradock_nginx_1 (05258db447edfcb766cb16b3895772c972128c4e68608317783257081467492a): Bind for 0.0.0.0:443 failed: port is already allocated
+ERROR: Encountered errors while bringing up the project.
+```
+
+then:
+```
+NGINX_HOST_HTTPS_PORT=4430
+```
+
+if:
+```
+Recreating laradock_nginx_1 ... error
+
+ERROR: for laradock_nginx_1  Cannot start service nginx: driver failed programming external connectivity on endpoint laradock_nginx_1 (60fe8fcd043577064431039ac9424d2a05c434deb198d9aa070aca4e9f223315): Bind for 0.0.0.0:80 failed: port is already allocated
+
+ERROR: for nginx  Cannot start service nginx: driver failed programming external connectivity on endpoint laradock_nginx_1 (60fe8fcd043577064431039ac9424d2a05c434deb198d9aa070aca4e9f223315): Bind for 0.0.0.0:80 failed: port is already allocated
+ERROR: Encountered errors while bringing up the project.
+```
+
+then:
+```
+NGINX_HOST_HTTP_PORT=800
+```
+
+## dockerhub
+```shell
+docker tag laradock_nginx:latest repetitor202/nginx:1.21.6-laradock-alpine3.15.0
+docker login
+docker push repetitor202/nginx:1.21.6-laradock-alpine3.15.0
+```
---
+++ script.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+Help()
+{
+  echo "Usage ./script.sh [OPTION]"
+  echo
+  echo "Syntax: ./script.sh [-b|e|h|n|q|s|u]"
+  echo "options:"
+  echo "-b     Build Laravel-environment (build mariadb, nginx, php-fpm, workspace)."
+  echo "-e     Prepare Laradock .env file."
+  echo "-h     Print this Help."
+  echo "-n     Rebuild (no-cache) Laravel-environment (build mariadb, nginx, php-fpm, workspace)."
+  echo "-q     Quick-Up Laravel-environment (build mariadb, nginx, php-fpm, workspace)."
+  echo "-s     Stop all containers."
+  echo "-u     Up Laravel-environment (build mariadb, nginx, php-fpm, workspace)."
+  echo
+}
+
+PrepareLaradockEnvFile()
+{
+  pathCurrentScript=$(dirname "$0")
+  pathProject="$pathCurrentScript"/laradock
+
+  cp  "$pathProject"/.env.example "$pathProject"/.env
+
+  sed -i 's@APP_CODE_PATH_HOST=../@APP_CODE_PATH_HOST=../../_support/boston-laravel/laravel/@' "$pathProject"/.env
+  sed -i 's@DATA_PATH_HOST=~/.laradock/data@DATA_PATH_HOST=~/.laradock/data-boston@' "$pathProject"/.env
+  sed -i 's@COMPOSE_PROJECT_NAME=laradock@COMPOSE_PROJECT_NAME=laradock-boston@' "$pathProject"/.env
+  sed -i 's@PHP_IDE_CONFIG=serverName=laradock@PHP_IDE_CONFIG=serverName=laradock-boston@' "$pathProject"/.env
+  sed -i 's@WORKSPACE_INSTALL_PHPREDIS=true@WORKSPACE_INSTALL_PHPREDIS=false@' "$pathProject"/.env
+  sed -i 's@WORKSPACE_INSTALL_MEMCACHED=true@WORKSPACE_INSTALL_MEMCACHED=false@' "$pathProject"/.env
+  sed -i 's@WORKSPACE_INSTALL_XDEBUG=false@WORKSPACE_INSTALL_XDEBUG=true@' "$pathProject"/.env
+  sed -i 's@PHP_FPM_INSTALL_XDEBUG=false@PHP_FPM_INSTALL_XDEBUG=true@' "$pathProject"/.env
+  sed -i 's@PHP_FPM_XDEBUG_PORT=9003@PHP_FPM_XDEBUG_PORT=9013@' "$pathProject"/.env
+}
+
+BuildLaravelEnvironment()
+{
+  PrepareLaradockEnvFile
+
+  pathCurrentScript=$(dirname "$0")
+  pathProject="$pathCurrentScript"/laradock
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- build -- mariadb nginx php-fpm workspace
+}
+
+RebuildNoCacheLaravelEnvironment()
+{
+  PrepareLaradockEnvFile
+
+  pathCurrentScript=$(dirname "$0")
+  pathProject="$pathCurrentScript"/laradock
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- build --no-cache -- mariadb nginx php-fpm workspace
+}
+
+QuickUpLaravelEnvironment()
+{
+  pathCurrentScript=$(dirname "$0")
+  pathProject="$pathCurrentScript"/laradock
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- up -d -- mariadb nginx php-fpm workspace
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace composer install
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace cp .env.example .env
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace  sed -i 's@DB_HOST=127.0.0.1@DB_HOST=mariadb@' .env
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace  sed -i 's@DB_DATABASE=laravel@DB_DATABASE=default@' .env
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace  sed -i 's@DB_USERNAME=root@DB_USERNAME=default@' .env
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace  sed -i 's@DB_PASSWORD=@DB_PASSWORD=secret@' .env
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace  php artisan key:generate
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace  php artisan migrate
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env \
+    -- exec --user=laradock -- workspace  php artisan test
+}
+
+UpLaravelEnvironment()
+{
+  BuildLaravelEnvironment
+  QuickUpLaravelEnvironment
+}
+
+Stop()
+{
+  pathCurrentScript=$(dirname "$0")
+  pathProject="$pathCurrentScript"/laradock
+
+  docker-compose -f "$pathProject"/docker-compose.yml --env-file "$pathProject"/.env -- stop
+}
+
+while getopts ":behnqsu" option; do
+   # shellcheck disable=SC2220
+   case $option in
+      b) BuildLaravelEnvironment
+         exit;;
+      e) PrepareLaradockEnvFile
+         exit;;
+      h) Help
+         exit;;
+      n) RebuildNoCacheLaravelEnvironment
+         exit;;
+      q) QuickUpLaravelEnvironment
+         exit;;
+      s) Stop
+         exit;;
+      u) UpLaravelEnvironment
+         exit;;
+   esac
+done
+
+Help
