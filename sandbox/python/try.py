3fd2a4fb5caf280bca602fac4b45246c1e6548c5
repetitor <repetitor202@gmail.com>
2022-03-21 22:58:57 +0700

    py - decorator

--- sandbox/python/try.py
+++ sandbox/python/try.py
@@ -9,3 +9,30 @@ first("Hello1")
 
 second = first
 second("Hello2-second")
+
+
+# decorator
+# https://www.programiz.com/python-programming/decorator
+def make_pretty(func):
+    def inner():
+        print("I got decorated")
+        func()
+
+    return inner
+
+
+def ordinary():
+    print("I am ordinary")
+
+
+ordinary = make_pretty(ordinary)
+ordinary()
+
+
+# equivalent to:
+@make_pretty
+def ordinary():
+    print("I am ordinary 2")
+
+
+ordinary()
