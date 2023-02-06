c064dc20c9d1a8d5d02c705018a0968aa26b149a
Viktar Akulenka <repetitor202@gmail.com>
2023-02-06 19:40:52 +0300

    trytelega

---
+++ sandbox/python/back-bot.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '6010132260:AAHCUMUAvpz_bsaQKavFa1vIzVa5izMw_CU'
+TEXT: str = 'Ура! Классный апдейт!'
+MAX_COUNTER: int = 100
+
+offset: int = -2
+counter: int = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
---
+++ sandbox/python/stepik-telega-bot.py
@@ -0,0 +1,13 @@
+import requests
+
+
+# api_url = 'http://api.open-notify.org/iss-now.json'
+# http://numbersapi.com/
+api_url = 'http://numbersapi.com/43'
+
+response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)    # При другом коде ответа выводим этот код
