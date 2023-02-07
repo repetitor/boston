e407edf382feb2e0dbf3ec8e4ef9b90172cf9195
Viktar Akulenka <repetitor202@gmail.com>
2023-02-07 12:33:21 +0300

    telega notify me

--- sandbox/python/back-bot.py
+++ sandbox/python/back-bot.py
@@ -5,12 +5,17 @@ import time
 API_URL: str = 'https://api.telegram.org/bot'
 BOT_TOKEN: str = '6010132260:AAHCUMUAvpz_bsaQKavFa1vIzVa5izMw_CU'
 TEXT: str = 'Ура! Классный апдейт!'
+TEXT202: str = 'кто-то прислал посыл)'
 MAX_COUNTER: int = 100
 
 offset: int = -2
 counter: int = 0
 chat_id: int
 
+chat_id202: int
+text_message: str
+chat_username: str
+txt_202: str
 
 while counter < MAX_COUNTER:
 
@@ -23,6 +28,14 @@ while counter < MAX_COUNTER:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
             requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+            chat_id202 = 394257307
+            # chat_username = result['message']['chat']['username']
+            chat_username = result['message']['chat']['last_name']
+            text_message = result['message']['text']
+            # txt_202 = TEXT202
+            txt_202 = 'username: ' + chat_username + ' message: ' + text_message
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id202}&text={txt_202}')
 
     time.sleep(1)
     counter += 1
